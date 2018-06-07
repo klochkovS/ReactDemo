cdbe7fb86c61e1663f5559e5afeca6c7581af6d9
klochkovS <fluurr@gmail.com>
2018-06-07 16:09:54 +0300

    add PeopleList Component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { v4 } from 'uuid';
 import AddColorForm from './AddColorForm';
 import ColorList from './ColorList';
-import TimeLine from './TimeLine';
+import PeopleList from './PeopleList';
 
 class App extends Component {
   constructor(props) {
@@ -42,52 +42,11 @@ class App extends Component {
   }
 
   render() {
-    const historicDatesForSkiing = [
-      {
-        year: 1879,
-        event: "Ski Manufacturing Begins"
-      },
-      {
-        year: 1882,
-        event: "US Ski Club Founded"
-      },
-      {
-        year: 1924,
-        event: "First Winter Olympics Held"
-      },
-      {
-        year: 1926,
-        event: "First US Ski Shop Opens"
-      },
-      {
-        year: 1932,
-        event: "North America's First Rope Tow Spins"
-      },
-      {
-        year: 1936,
-        event: "First Chairlift Spins"
-      },
-      {
-        year: 1949,
-        event: "Squaw Valley, Mad River Glen Open"
-      },
-      {
-        year: 1958,
-        event: "First Gondola Spins"
-      },
-      {
-        year: 1964,
-        event: "Plastic Buckle Boots Available"
-      }
-    ];
     const { addColor, rateColor, removeColor } = this;
     const { colors } = this.state;
     return (
       <div className="app">
-        <TimeLine
-          name="History of Skiing"
-          data={historicDatesForSkiing}
-        />
+        <PeopleList />
         <AddColorForm onNewColor={addColor} />
         <ColorList
           colors={colors}
