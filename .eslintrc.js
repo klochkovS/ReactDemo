2b219ae30a7ee8baa7365be110faa7024b76878c
klochkovS <fluurr@gmail.com>
2018-06-11 10:46:07 +0300

    add reducers

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         "no-nested-ternary": 0,
         "react/self-closing-comp": 0,
         "no-confusing-arrow": 0,
+        "import/prefer-default-export": 0,
     },
     "env": {
         "browser": 1
